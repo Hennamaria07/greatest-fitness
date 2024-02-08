75b5066ec7fcb56925f16118f647e863a7e9dca2
Henna Maria <hennamaria2001@gmail.com>
2024-02-08 14:17:28 +0530

    base url updated

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:4000/api/v1"
+    baseURL: "https://great-fitness-backend.onrender.com"
 });
 
 export default instance;
