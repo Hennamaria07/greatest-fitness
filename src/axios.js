938f9f47333cd66c7506ff0d53773a932b15d9ff
Henna Maria <hennamaria2001@gmail.com>
2024-02-08 14:59:51 +0530

    debugging v1

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://great-fitness-backend.onrender.com"
+    baseURL: "https://great-fitness-backend.onrender.com",
 });
 
 export default instance;
